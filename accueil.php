37d5a9ddd54ea1bd82a7a4441dc0e6bfdad3faa0
EvanBRISSARD <evanbrissard750@gmail.com>
2026-05-02 21:28:16 +0200

    Configuration première

---
+++ accueil.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head> <!-- 1. Contient les métadonnées (invisible pour l'utilisateur) -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Important pour le mobile ! -->
+    <title>Accueil - Mon Site Web</title> <!-- Très important pour le SEO -->
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+
+    <header class="header-container"> 
+        <h1 class="head-h1">Accueil</h1>
+        <nav class="container-nav">
+            <ul>
+                <li><a href="index.html">Accueil</a></li>
+                <li><a href="classeur.html">Classeur</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <p>Bienvenue sur mon site !</p>
+    </main>
+
+    <footer>
+        <p>&copy; 2026 Mon Site Web. Tous droits réservés.</p>
+    </footer>
+
+</body>
+</html>

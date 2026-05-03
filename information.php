d0066dce098dec12fcd1fae575d9eca415d1e288
EvanBRISSARD <evanbrissard750@gmail.com>
2026-05-03 19:18:37 +0200

    mise a jour titre information.php

--- information.php
+++ information.php
@@ -30,7 +30,7 @@ $candidaturesEntreprise = getCandidaturesParEntrepriseId($entreprise['id'], $can
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?> - Tableau de Bord</title>
+    <title><?php echo $title; ?> - <?php echo $entreprise['nom']; ?></title>
     <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="style/styleDetails.css">
     <link rel="stylesheet" href="style/styleTag.css">

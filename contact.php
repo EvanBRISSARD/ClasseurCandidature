062576c5bd2d902dc166be56ea48a893b0437cdf
EvanBRISSARD <evanbrissard750@gmail.com>
2026-05-05 22:09:15 +0200

    modification contact titre

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@ $db = getPDO();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title; ?> - Tableau de Bord</title>
+    <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="style/StyleEntrer.anima.css">
     <link rel="stylesheet" href="style/styleFrom.css">
